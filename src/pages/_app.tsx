aa7dbd8bcc0d12182f585f3ab862c56f2160a637
Diseyi <filomiinaa@gmail.com>
2023-08-09 01:37:26 +0100

    chore: Install Chakra UI, switch package manager to Yarn and default code from Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
+import { Inter } from 'next/font/google';
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme } from '@chakra-ui/react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+// Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+export const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+        }
+      `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+
+  )
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+/* theme.ts */
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+    fonts: {
+      heading: 'var(--font-inter)',
+      body: 'var(--font-inter)',
+    }
+});
