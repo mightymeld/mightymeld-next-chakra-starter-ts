028938b36097cb4497ba7c813e799a30bdc81bbf
Diseyi <filomiinaa@gmail.com>
2023-10-20 11:29:04 +0100

    update plugin to swc replace .env with mightymeld.secrets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { pluginoptions } = require('@mightymeld/runtime');
+
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: [['@mightymeld/runtime/swc-plugin-mightymeld', pluginoptions()]]
+  }
 };
 
-module.exports = nextConfig;
+module.exports = process.env.MIGHTYMELD ? nextConfig : {};
